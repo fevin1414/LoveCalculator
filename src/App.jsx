2d84bfba0358a6ea8e89df07d4234d8d5cc5fba0
fevin1414 <fevinbiju1414@gmail.com>
2024-03-30 01:20:15 -0500

    added UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import "./index.css";
+import Modal from "./Components/Modal";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return <></>;
+  function openModal() {
+    const modalDialog = document.getElementById("my_modal_5");
+    if (modalDialog) {
+      modalDialog.open = true;
+      setIsModalOpen(true);
+    }
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
+
+  return (
+    <>
+      <div className="hero min-h-screen bg-red-500 flex flex-col justify-start items-center pt-20">
+        <h1 className="text-6xl font-bold love-ya-like-a-sister-regular flex items-center text-white">
+          <span className="mr-2">LOVE</span>
+          <span className="text-pink-500">
+            <FontAwesomeIcon
+              icon={faHeart}
+              className="ml-5 mr-5"
+              style={{ color: "white" }}
+              beat
+            />
+          </span>
+          <span className="ml-2">CALCULATOR</span>
+        </h1>
+        <div className="flex justify-center mt-20">
+          <input
+            type="text"
+            placeholder="Your name"
+            className="input input-bordered rounded-lg shadow-lg w-full madimi-one-regular max-w-md mr-4 text-lg"
+          />
+          <input
+            type="text"
+            placeholder="His/Her name"
+            className="input input-bordered rounded-lg shadow-lg madimi-one-regular w-full max-w-md ml-4 text-lg"
+          />
+        </div>
+
+        <Modal displayModal={openModal} />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+const Modal = ({ displayModal }) => {
+  const closeModal = () => {
+    const modalDialog = document.getElementById("my_modal_5");
+    if (modalDialog) {
+      modalDialog.close();
+    }
+  };
+
+  return (
+    <div>
+      <button
+        onClick={displayModal}
+        className="btn bg-white text-red-500 madimi-one-regular mt-6 px-8 py-3 text-lg"
+      >
+        Calculate ❤️
+      </button>
+      <dialog
+        id="my_modal_5"
+        className="modal modal-bottom sm:modal-middle"
+        open={false}
+      >
+        <div className="modal-box">
+          <button
+            onClick={closeModal}
+            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          <h3 className="font-bold text-lg madimi-one-regular text-center">
+            Your Result
+          </h3>
+          <div className="flex justify-center mt-5">
+            <input
+              type="text"
+              disabled
+              placeholder="Your name"
+              className="input input-bordered rounded-lg shadow-lg w-full madimi-one-regular max-w-md mr-4 text-lg"
+            />
+            <span className="text-pink-500">
+              <FontAwesomeIcon
+                icon={faHeart}
+                className="ml-5 mr-5 mt-3"
+                style={{ color: "red", fontSize: "28px" }}
+                beat
+              />
+            </span>
+            <input
+              type="text"
+              placeholder="His/Her name"
+              disabled
+              className="input input-bordered rounded-lg shadow-lg madimi-one-regular w-full max-w-md ml-4 text-lg"
+            />
+          </div>
+          <div className="radial-progress-wrapper flex justify-center mt-5">
+            <div
+              className="radial-progress bg-primary text-primary-content border-4 border-primary"
+              style={{ "--value": 70 }}
+              role="progressbar"
+            >
+              70%
+            </div>
+          </div>
+          <p className="text-lg font-bold mt-3 text-center love-ya-like-a-sister-regular">
+            Result
+          </p>
+        </div>
+      </dialog>
+    </div>
+  );
+};
+
+export default Modal;
